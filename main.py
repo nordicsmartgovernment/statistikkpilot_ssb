30fa801f4573702ff681440308ab9f690d8015f2
Skagemo <5012227+sskagemo@users.noreply.github.com>
2023-02-22 15:16:39 +0100

    steg 1 - velge og lese fil ok

---
+++ main.py
@@ -0,0 +1,82 @@
+import asyncio
+import io
+import json
+
+import pandas as pd
+from js import FileReader, document
+from pyodide import create_proxy
+from pyodide.http import open_url
+from request import request  # request-funksjonen fra pyscript, siden 'requests' mangler
+from pyscript import Element
+
+# egne moduler
+from saft2dataframe import saft2dataframe
+
+  
+# Koden for å lese fil er hentet fra her:
+# https://github.com/amrrs/pyscript-file-uploader/blob/main/index.html
+
+saft_innhold = ""  # global variabel, teksten fra saft-fila
+saft = pd.DataFrame()  # global variabel
+
+def hent_saft_innhold() -> io.StringIO:
+    if saft_innhold == "":
+        print('Du må først velge en lokal fil')
+        return None
+    else:
+#        print(f'Her er saft_innhold: {saft_innhold[:100]}')
+        return io.StringIO(saft_innhold)
+
+
+def read_complete(event) -> pd.DataFrame:
+# event is ProgressEvent
+
+    content = document.getElementById("content")
+    global saft_innhold, saft
+    saft_innhold = event.target.result
+#    print(f'Har oppdatert saft_innhold: {saft_innhold[:100]}')
+
+
+    saft = saft2dataframe(hent_saft_innhold())
+#    print(f'''har oppdatert saft dataframe, med innholdet i saft-fila som begynner med {saft_innhold[:100]}
+#
+#          Variabelen saft har følgende kolonner: {saft.columns}''')
+
+    #document.getElementById('a2').style.display = 'block'  >> erstattet med pyscript-funksjonaliteten under, med element
+    el = Element('resultat_a1')
+    el.write('Filen er lest, gå til neste steg')
+
+    el = Element('a2')
+    el.remove_class('hidden')
+
+
+async def process_file(x):
+    fileList = document.getElementById('saftfil').files
+
+    for f in fileList:
+        # reader is a pyodide.JsProxy
+        reader = FileReader.new()
+
+        # Create a Python proxy for the callback function
+        onload_event = create_proxy(read_complete)
+
+        #console.log("done")
+
+        reader.onload = onload_event
+
+        reader.readAsText(f)
+
+        return  # lurer litt på hvorfor det er return inne i for-løkken ... kanskje en typo av meg?
+
+
+def oppsett_for_lesing_av_lokal_fil():
+    # Create a Python proxy for the callback function
+    file_event = create_proxy(process_file)
+
+    # Set the listener to the callback
+    e = document.getElementById("saftfil")
+    e.addEventListener("change", file_event, False)
+
+
+oppsett_for_lesing_av_lokal_fil()
+   
---
+++ saft2dataframe.py
@@ -0,0 +1,125 @@
+# Leser en SAF-T-fil og lager en Pandas DataFrame av data
+# fra alle "Transaction"-elementene
+# 
+# Dokumentasjon av ET: https://docs.python.org/3/library/xml.etree.elementtree.html
+#
+# Known bugs:
+# Hopper over "Analysis"-elementet som det kan være flere av pr linje
+
+
+import pandas as pd
+import xml.etree.ElementTree as ET
+from typing import List
+
+def saft2dataframe(file: str) -> pd.DataFrame:
+    '''
+        leser en SAF-T Financial NO-fil og returnerer alle "Transaction"
+        som en pandas DataFrame
+    '''
+    if file == None:
+        print("Mangler en fil, avslutter, returnerer None")
+        return None
+        
+    tree = ET.parse(file)
+
+    df = pd.DataFrame()
+
+    journal = tree.getroot().find('*/{urn:StandardAuditFile-Taxation-Financial:NO}Journal')
+    # Note: Can be useful to have data about the journal later. And the organisation etc.
+    # But focusing on the transactions first ...
+
+    transactions = journal.findall('{urn:StandardAuditFile-Taxation-Financial:NO}Transaction')
+
+    for transaction in transactions:
+        df = pd.concat([df, pd.DataFrame(process_transaction(transaction))])
+
+    df = df.convert_dtypes() # gjør om de fleste fra object til string
+    df = df.astype({ # valg av datatype pr felt bør verifiseres av noen ...
+    #    'Transaction.Transaction': str,
+    #    'Transaction.TransactionID': str,
+        'Transaction.Period': int,
+        'Transaction.PeriodYear': int,
+        'Transaction.TransactionDate': 'datetime64',
+        'Transaction.TransactionType': 'category',
+    #    'Transaction.Description': str,
+        'Transaction.SystemEntryDate': 'datetime64',
+        'Transaction.GLPostingDate': 'datetime64',
+    #    'Line.Line': str,
+    #    'Line.RecordID': str,
+        'Line.AccountID': int,
+    #    'Line.Analysis': 'str',
+    #    'Line.AnalysisType': 'category',
+    #    'Line.AnalysisID': 'category',
+    #    'Line.AnalysisAmount': str,
+    #    'Line.Amount': float, # OOOOOOPs ... kan være både Tax Amount, og Analysis Amount
+        'Line.ValueDate': 'datetime64',
+    #    'Line.SourceDocumentID': str,
+    #    'Line.Description': str,
+    #    'Line.DebitAmount': str,
+        'Line.DebitAmount.Amount': float,
+    #    'Line.TaxInformation': str,
+        'Line.TaxType': 'category',
+        'Line.TaxCode': 'category',
+        'Line.TaxPercentage': float,
+        'Line.TaxBase': float,
+        'Line.TaxAmount.Amount': float, # Her skulle det kommet en "Amount" rett etterpå ...
+    #    'Line.ReferenceNumber': str,
+    #    'Line.SupplierID': str,
+        'Line.CreditAmount.Amount': float,
+    #    'Line.CustomerID': str,
+    })
+    return df
+
+
+def process_line(line: ET.Element) -> dict:
+    '''lager dict for en line
+        NB!! Ignorerer "Analysis"-elementene'''
+
+    analysis = line.findall('{urn:StandardAuditFile-Taxation-Financial:NO}Analysis')
+    for analyse in analysis:
+        line.remove(analyse)
+
+    res = {}
+    for ld in line.iter():
+        name = 'Line.' + str(ld.tag).split('}')[1]
+        if name == 'Line.Amount':
+            name = list(res.keys())[-1] + '.' + name.split('.')[-1]
+
+        res[name] = ld.text.strip() if ld.text.strip() != '' else None
+
+    return res
+
+
+def process_transaction(transaction: ET.Element) -> List[dict]:
+    '''lager en liste av dicts om hver transaction
+    dict-listen kan legges til en dataframe'''
+
+    resultat = []
+    transaction_res = {}
+    line_res = []
+    lines = transaction.findall('{urn:StandardAuditFile-Taxation-Financial:NO}Line')
+    for line in lines:
+        line_res.append(process_line(line))
+        transaction.remove(line)
+
+    for transaction_data in transaction.iter(): 
+        name = 'Transaction.' +  str(transaction_data.tag).split('}')[1]
+        transaction_res[name] = transaction_data.text.strip() if \
+            transaction_data.text.strip() != '' else None
+    
+    if len(line_res) == 0:
+        resultat.append(transaction_res)
+
+    else:
+        for line in line_res:
+            resultat.append({**transaction_res, **line})        
+
+    return resultat
+
+
+if __name__ == '__main__':
+    df = saft2dataframe('saft.xml')
+    print(df.describe())
+    print(df.columns)
+    print(df.info())
+    print(df)
